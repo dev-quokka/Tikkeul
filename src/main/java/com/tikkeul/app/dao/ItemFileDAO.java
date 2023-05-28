122624707479457a495ddff47e525afa4ddaf3b1
YunkiHong <>
2023-05-29 05:27:58 +0900

    05-29

--- src/main/java/com/tikkeul/app/dao/ItemFileDAO.java
+++ src/main/java/com/tikkeul/app/dao/ItemFileDAO.java
@@ -1,7 +1,10 @@
 package com.tikkeul.app.dao;
 
+import com.tikkeul.app.domain.vo.ItemFileVO;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ItemFileDAO {
+    public void saveItem(ItemFileVO itemFileVO) {
+    }
 }
--- src/main/java/com/tikkeul/app/domain/vo/FileVO.java
+++ src/main/java/com/tikkeul/app/domain/vo/FileVO.java
@@ -14,4 +14,5 @@ public class FileVO {
     private String uuid;
     private String path;
     private String fileType;
+
 }
